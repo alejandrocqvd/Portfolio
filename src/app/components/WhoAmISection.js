00577d3aaee227ef6613dfc58bb788dc56bd3a39
alejandrocqvd <alejandrocqvd@gmail.com>
2024-05-20 14:49:01 -0600

    yipe

--- src/app/components/WhoAmISection.js
+++ src/app/components/WhoAmISection.js
@@ -12,7 +12,7 @@ const WhoAmISection = () => {
     <section ref={ref} className={`relative -top-36 ${inView ? 'section-visible' : 'section-hidden'}`}>
       <p className="text-5xl md:text-7xl font-bold text-center mb-10 md:mb-20 mt-12">Who Am I?</p>
       <div className="flex flex-col md:flex-row justify-center items-center w-9/12 md:w-8/12">
-        <img src="/portrait.png" className="h-24 md:h-96 w-auto object-cover white-box-shadow md:flex-1 mb-10 md:mb-0 md:mr-8" alt="Portrait" />
+        <img src="/portrait.png" className="h-96 w-auto object-cover white-box-shadow md:flex-1 mb-10 md:mb-0 md:mr-8" alt="Portrait" />
         <div className="flex flex-col justify-center items-center md:flex-1 md:ml-8">
           <p className="text-xl">
             I am a full time student currently enrolled in Computer Science at 
--- src/app/components/WorkShowcase.js
+++ src/app/components/WorkShowcase.js
@@ -12,8 +12,8 @@ export default function WorkShowcase(props) {
   return (
     <section ref={ref} className={inView ? 'section-visible' : 'section-hidden'}>
       <div className={`flex ${ reversed ? "md:flex-row-reverse" : "md:flex-row"} w-full flex-col justify-center items-center mb-24 md:mb-32`}>
-        <img src={image} alt={title} className={`flex-1 h-24 md:h-72 w-auto m-auto mb-10 md:mb-0 object-cover white-box-shadow ${ reversed ? "md:ml-8" : "md:mr-8"}`}/>
-        <div className={`flex flex-col flex-1 h-full justify-center text-md md:text-lg ${ reversed ? "md:mr-8" : "md:ml-8"}`}>
+        <img src={image} alt={title} className={`md:flex-1 h-72 w-auto m-auto mb-10 md:mb-0 object-cover white-box-shadow ${ reversed ? "md:ml-8" : "md:mr-8"}`}/>
+        <div className={`flex flex-col md:flex-1 h-full justify-center text-md md:text-lg ${ reversed ? "md:mr-8" : "md:ml-8"}`}>
           { link ? <a href={link} target="_blank" className="text-2xl md:text-4xl font-bold mb-2 hover:underline">{title}</a> : <p className="text-2xl md:text-4xl font-bold mb-2">{title}</p>}
           <p>{subtitle}<br></br><br></br></p>
           <p><span className="font-bold">GOAL: </span>{description}<br></br><br></br></p>
