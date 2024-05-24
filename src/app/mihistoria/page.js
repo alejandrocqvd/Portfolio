454f5b7695ae18e0ae7091b4d3ebc736d83ea592
alejandrocqvd <alejandrocqvd@gmail.com>
2024-05-24 01:12:51 -0600

    started mihistoria page

---
+++ src/app/mihistoria/page.js
@@ -0,0 +1,56 @@
+export default function MiHistoria() {
+  return (
+    <div className="w-full justify-center items-center">
+      <div className="flex flex-col justify-center items-center w-8/12 m-auto my-16">
+
+        <h1 className="font-bold text-6xl text-center mb-10 bg-clip-text text-transparent bg-gradient">MiHistoria</h1>
+
+        {/* <div className="flex flex-row justify-center items-center">
+          <a 
+            href="https://github.com/alejandrocqvd/MiHistoria" 
+            target="_blank" 
+            className="flex flex-row justify-between items-center w-32 shadow-md rounded-xl px-4 py-2 mr-3 hover:opacity-70"
+          >
+            <p className="text-lg">GitHub</p>
+            <img 
+              src="/github-logo.png" 
+              alt="GitHub logo" 
+              className="h-6 w-auto object-cover" 
+            />
+          </a>
+          <a 
+            href="https://mihistoria.vercel.app/" 
+            target="_blank" 
+            className="flex flex-row justify-between items-center w-32 shadow-md rounded-xl px-4 py-2 ml-3 hover:opacity-70"
+          >
+            <p className="text-lg">Website</p>
+            <img 
+              src="/website-icon.png" 
+              alt="Website icon" 
+              className="h-6 w-auto object-cover" 
+            />
+          </a>
+        </div>
+
+        <img 
+          src="/mihistoria-img.png" 
+          alt="MiHistoria Landing Page"
+          className="h-auto w-10/12 white-box-shadow rounded-xl my-10"
+        /> */}
+
+        <div className="flex flex-row justify-center items-center">
+          <p className="flex-1 text-lg mr-6">
+            MiHistoria is a social media web application I designed and developed to continue developing the skills I learnt from working on DinoDB. 
+            On MiHistoria, each user gets one, and only one post, and in that post they write their life story. Here, users can like, comment on, and 
+            save others' stories, fostering a sense of community and engagement. Additionally, users have the option to write their stories anonymously or publicly, 
+            providing flexibility and privacy as desired.
+          </p>
+          <p className="flex-1 ml-6">
+          yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap 
+          </p>
+        </div>
+
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,13 @@ module.exports = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
         redHat: ['"Red Hat Display"', 'sans-serif'],
+      },
+      backgroundImage: {
+        'gradient': 'linear-gradient(90deg, rgba(102,0,204,1) 0%, rgba(179,0,89,1) 100%)',
       },
     },
   },
