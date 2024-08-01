b5d57e0f8f58632eb2f3226961158100d339f61e
alejandrocqvd <alejandrocqvd@gmail.com>
2024-07-31 21:44:38 -0600

    fixed spacing on nav links

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Navbar />
 
       <div id="home"></div>
-      <div className="flex flex-col justify-center items-center text-center w-screen md:w-full h-screen landing-page-bg bg-cover bg-no-repeat">
+      <div className="flex flex-col justify-center items-center text-center w-screen md:w-full h-screen landing-page-bg bg-cover bg-no-repeat mb-20">
         <h1 className="text-4xl md:text-8xl">ALEJANDRO</h1>
         <h1 className="text-4xl md:text-8xl mt-4 mb-10">CARDONA</h1>
         <div className="w-96 overflow-hidden relative whitespace-nowrap">
@@ -18,7 +18,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div id="about" className="relative -top-40 -z-10 h-32 md:h-40"></div>
+      <div id="about" className="relative -top-40 -z-10 h-32 md:h-28"></div>
       <WhoAmISection />
 
       <div id="skills"  className="relative -top-10 -z-10 h-20 md:h-40"></div>
