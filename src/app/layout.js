8337c43eed8291e67491df65db8a9a4f5bf0b48f
alejandrocqvd <alejandrocqvd@gmail.com>
2024-08-24 17:19:51 -0600

    trying to fix font not working

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-import { Inter } from "next/font/google";
 import "../globals.css";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +13,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Notable&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
