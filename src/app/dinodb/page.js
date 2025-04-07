7a201d8540545d23fb429ab1bf288bcf1957720d
alejandrocqvd <alejandrocqvd@gmail.com>
2025-04-07 00:08:05 -0600

    XD

--- src/app/dinodb/page.js
+++ src/app/dinodb/page.js
@@ -1,4 +1,15 @@
-import SkillBox from "../../components/SkillBox";
+const SkillBox = (props) => {
+  const { skillName, skillImage, bgColour, rounded } = props;
+  
+  return (
+    <div className={`skill-box h-12 xl:h-16 w-full ${bgColour} ${rounded == 1 ? "rounded-l-lg" : (rounded == 2 ? "rounded-r-lg" : "") }`}>
+      <div className="blur-container">
+        <img className="skill-image h-full w-full" src={skillImage} alt={skillName} />
+      </div>
+      <p className="skill-text font-normal xl:font-bold text-sm xl:text-md drop-shadow-2xl">{skillName}</p>
+    </div>
+  );
+}
 
 export default function DinoDB() {
   return (
